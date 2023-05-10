86c32d50a134d75fd9c624d9d4ba1b4520452e59
23Alanes7 <aok0033426@est.univalle.edu>
2023-05-10 14:13:08 -0400

    Hola Mundo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Alanes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int n1 = 2;
+            int n2 = 3;
+            int result = n1 + n2;
+            Console.WriteLine(result);
+        }
+    }
+}
